dc973b1a083c0cfc8f65a8d0a1a2fd4e7c321334
Amzad hossain <amzad.rafi@northsouth.edu>
2023-01-08 21:26:43 +0600

    ..

---
+++ app.py
@@ -0,0 +1,19 @@
+#import flask 
+from flask import Flask, render_template, request, redirect, url_for, flash 
+
+
+
+
+
+
+
+# Create an instance of Flask 
+app = Flask(__name__) 
+#home page 
+@app.route('/') 
+def home(): 
+    return render_template('index.html') 
+
+if __name__ == '__main__': 
+    app.run(debug=True) 
+    
