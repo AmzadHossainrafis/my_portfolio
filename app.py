a54bab04d985d3666f3d0ce2b6ad9916bf55ece8
Amzad hossain <amzad.rafi@northsouth.edu>
2023-02-05 21:09:27 +0600

    new cir added some fine tune some section

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #import flask 
 from flask import Flask, render_template, request, redirect, url_for, flash 
-
+import os 
+import sqlite3
 
 
 
@@ -13,6 +14,12 @@ app = Flask(__name__)
 @app.route('/') 
 def home(): 
     return render_template('home.html') 
+
+# login page 
+@app.route('/loin') 
+def login(): 
+
+    return render_template('login.html')  # render a template
 
 if __name__ == '__main__': 
     app.run(debug=True) 
